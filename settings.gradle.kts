32b0579f06f7d5d46792b9687052441a2dd0b6e4
Ludovic Lonlas <ludovic.lonlas@etu.umontpellier.fr>
2023-06-14 10:56:51 +0200

    Publish Build Scans from GitHub Actions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,20 @@ pluginManagement {
         kotlin("multiplatform").version(extra["kotlin.version"] as String)
         id("org.jetbrains.compose").version(extra["compose.version"] as String)
     }
+
+}
+plugins {
+    id("com.gradle.enterprise") version("3.9")
 }
 
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
 rootProject.name = "Portfolio"
 
