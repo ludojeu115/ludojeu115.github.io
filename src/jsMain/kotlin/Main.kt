40c4894dcb2888dd8714257c3d6f009176f7a725
Ludovic Lonlas <ludovic.lonlas@etu.umontpellier.fr>
2023-06-25 14:45:59 +0200

    Retour androidx compose + html compose et progression de la page web.

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,8 +1,43 @@
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.Window
+import org.jetbrains.compose.web.attributes.height
+import org.jetbrains.compose.web.attributes.width
+import org.jetbrains.compose.web.dom.*
+import org.jetbrains.compose.web.renderComposable
 import org.jetbrains.skiko.wasm.onWasmReady
 fun main() {
 
-    onWasmReady {
-        BrowserViewportWindow("My Compose Application") {
+
+    renderComposable(rootElementId = "root") {
+        //Create canvas of size 600 400 with name ComposeTarget
+
+        Header({classes("primary")}){//Portfolio
+            Div({classes("Ludo","on-primary-text","title-medium")}){
+                H1{ Text("Ludovic Lonlas") }
+            }
+            Div({classes("Infos","on-primary-text","title-small")}){
+                H2{ Text("CV") }
+                H2{ Text("Projets") }
+                H2{ Text("Compétences") }
+                H2{ Text("Formations") }
+            }
+
+        }
+        Div({classes("blurred-picture")})
+        Img(src = "images/Ludo.png",alt = "Photo de Ludovic Lonlas"){classes("picture")}
+
+        Canvas({//Composable canvas
+            width(600)
+            height(400)
+            id ("ComposeTarget")
+
+        })
+
+    }
+    onWasmReady {//launch composable when wasm is ready
+        Window {
             MainPage()
         }
     }
--- src/jsMain/kotlin/MainPage.kt
+++ src/jsMain/kotlin/MainPage.kt
@@ -1,11 +1,47 @@
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
 @Composable
 fun MainPage() {
-    Column {
-        Text("Hello World!", color = Color.Red)
-    }
+        Column(modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState())
+
+            , horizontalAlignment = Alignment.CenterHorizontally) {
+            Text("Hello World!", color = Color.Red)
+            Button(onClick = { println("Button clicked!") }) {
+                Text("Click me!")
+            }
+            Button(onClick = { println("Button clicked!") }) {
+                Text("Click me!")
+            }
+            Button(onClick = { println("Button clicked!") }) {
+                Text("Click me!")
+            }
+            Button(onClick = { println("Button clicked!") }) {
+                Text("Click me!")
+            }
+            Button(onClick = { println("Button clicked!") }) {
+                Text("Click me!")
+            }
+            Button(onClick = { println("Button clicked!") }) {
+                Text("Click me!")
+            }
+            Button(onClick = { println("Button clicked!") }) {
+                Text("Click me!")
+            }
+            Button(onClick = { println("Button clicked!") }) {
+                Text("Click me!")
+            }
+            Button(onClick = { println("Button clicked!") }) {
+                Text("Click me!")
+            }
+        }
 }
